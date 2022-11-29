0416426f846f669bd4dcb4d2cc739c8003889d7b
Nandhini S R <49654911+nandhini-sr@users.noreply.github.com>
2022-11-29 12:17:34 +0530

    Using EmbeddedDialogHost style
    Using MainWindow.xaml - <views:DataGridViewEmbedded/> will execute this file.

---
+++ Views/DataGridViewEmbedded.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataGridDemo.Views
+{
+	/// <summary>
+	/// Interaction logic for DataGridViewEmbedded.xaml
+	/// </summary>
+	public partial class DataGridViewEmbedded : UserControl
+	{
+		public DataGridViewEmbedded()
+		{
+			InitializeComponent();
+		}
+	}
+}
