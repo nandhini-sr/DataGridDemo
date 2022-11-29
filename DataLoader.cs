ae43f251ab738c9b0c790bb0fe64837b5149770e
Nandhini S R <49654911+nandhini-sr@users.noreply.github.com>
2022-11-29 11:41:32 +0530

    Data Grid Demo - Initial files

---
+++ DataLoader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridDemo
+{
+	public class DataLoader
+	{
+		public class EachSearchResult
+		{
+			public string Description { get; set; }
+			public int TestNumber { get; set; }
+			public string TestFacility { get; set; }
+
+		}
+
+		public List<EachSearchResult> SearchResults { get; set; }
+
+		public DataLoader()
+		{
+			SearchResults = new List<EachSearchResult>();
+			for (int i = 1; i < 10; i++)
+			{
+				EachSearchResult sr = new EachSearchResult();
+				sr.Description = "Description" + (i*i+4).ToString();
+				sr.TestNumber = i * i + 23 * i + 358;
+				sr.TestFacility = "Facility" + (2*(10-i)+3).ToString();
+				SearchResults.Add(sr);
+			}
+		}
+	}
+}
